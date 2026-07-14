9945a95e0a73b9917fca6703994fec5657f98917
Daniel Diliberto <daniel.diliberto@hotmail.com>
2026-07-14 13:51:27 -0400

    Add: design-browser-history - submission-3

---
+++ Data Structures & Algorithms/design-browser-history/submission-3.ts
@@ -0,0 +1,41 @@
+class BrowserHistory {
+    stack: string[];
+    index: number;
+    /**
+     * @constructor
+     * @param {string} homepage
+     */
+    constructor(homepage) {
+        this.stack = [];
+        this.stack.push(homepage)
+        this.index = 0;
+    }
+
+    /**
+     * @param {string} url
+     * @return {void}
+     */
+    visit(url) {
+        this.stack.splice(this.index + 1)
+        this.stack.push(url);
+        this.index = this.stack.length - 1;
+    }
+
+    /**
+     * @param {number} steps
+     * @return {string}
+     */
+    back(steps) {
+        this.index = this.index - steps > 0 ? this.index - steps : 0;
+        return this.stack[this.index];
+    }
+
+    /**
+     * @param {number} steps
+     * @return {string}
+     */
+    forward(steps) {
+        this.index = this.index + steps < (this.stack.length - 1) ? this.index + steps : this.stack.length - 1;
+        return this.stack[this.index]
+    }
+}
