b11a1a4f421deb8f07a4d4421afc892865fe5c09
Daniel Diliberto <daniel.diliberto@hotmail.com>
2026-07-17 23:12:46 -0400

    Add: implement-stack-using-queues - submission-4

---
+++ Data Structures & Algorithms/implement-stack-using-queues/submission-4.ts
@@ -0,0 +1,45 @@
+class MyStack {
+    stack: number[]
+
+    constructor() {
+        this.stack = []
+    }
+
+    /**
+     * @param {number} x
+     * @return {void}
+     */
+    push(x: number): void {
+        this.stack = [x, ...this.stack]
+    }
+
+    /**
+     * @return {number}
+     */
+    pop(): number {
+        return this.stack.shift()
+    }
+
+    /**
+     * @return {number}
+     */
+    top(): number {
+        return this.stack[0];
+    }
+
+    /**
+     * @return {boolean}
+     */
+    empty(): boolean {
+        return !this.stack.length;
+    }
+}
+
+/**
+ * Your MyStack object will be instantiated and called as such:
+ * var obj = new MyStack()
+ * obj.push(x)
+ * var param_2 = obj.pop()
+ * var param_3 = obj.top()
+ * var param_4 = obj.empty()
+ */
